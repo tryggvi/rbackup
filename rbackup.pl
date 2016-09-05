a1e42813ef3b4c5d4ce8c665badcbe82212f2c21
root <root@dash.mosar.lan>
2016-09-05 17:55:48 +0000

    Major changes for version 2

--- rbackup.pl
+++ rbackup.pl
@@ -8,176 +8,179 @@
 # Author: Tryggvi Farestveit <trygvi@ok.is>
 #############################
 use strict;
-use File::Temp qw/ tempdir /;
+use Data::Dumper;
+use Getopt::Long;
 
 #### Settings
-#
-# Server information
-my $server = "backup.example.com";
-my $ssh_user = "backup";
-my $context = "dev.example.com";
-my $ssh_key = ""; # Use other than default SSH key
-my $ssh_known_hosts = ""; # Use other known host file than default
-
-# Notification
-my $email=0; # Send email when finished
-my $email_to = "test\@domain.com";
-my $email_from = "Backup client <root\@domain.com>";
-my $email_subject = "Rsync backup for {HOSTNAME}"; # {HOSTNAME} will be replaced for hostname of the server
-
-# Logging
-my $logfile = "/var/log/rbackup.log"; # Log file
-my $outfile = "/var/log/rbackup-out.log"; # Output of rsync cmd for last session
-
-# Directories to backup
-my @backup_dir = (
-	"/"
-);
-
-# Files and directories to exclude
+# Global excludes
 my @exclude = (
-	"/tmp",
 	"/proc",
 	"/sys",
 	"/dev",
 );
 
+## Hosts to backup
+my %hosts = (
+	"test.example.com" => {
+		"user" => "root",
+		"include" => {
+			"/etc/",
+			"/home/"
+		},
+		"exclude" => {
+			"/tmp/",
+		}
+	},
+	"temp.example.com" => {
+		"user" => "backup",
+		"include" => {
+			"/",
+		},
+		"exclude" => {
+			"/tmp/",
+		}
+	}
+);
+
+
+# Server information
+my $ssh_key = ""; # Use other than default SSH key
+my $ssh_known_hosts = ""; # Use other known host file than default
+my $backup_dir = "/data/backup";
+
+# Logging
+my $logdir = "/var/log/rbackup";
+my $logfile = $logdir."/rbackup.log"; # Log file
+
 # Misc
-my $options = "-v -a -R -z --delete --force --ignore-errors";
+my $options = "-v --info=BACKUP,COPY,DEL -a -R -z --delete --force --ignore-errors";
 my $debug = 0;
 
 ########### Do not edit below ###########
-open(LOG, ">>$logfile");
-printlog("===========================");
-printlog("Starting backup");
-my @timestruct = (localtime(time()));
-my $weekday = $timestruct[6];
-my $weekday_name = getWeekDay($weekday);
+my ($o_verb, $o_help, $o_test, $o_debug, $o_run);
 
-# Prep backup dirs
-my $bdir;
-for(my $i=0; $i < scalar(@backup_dir); $i++){
-	my $line = $backup_dir[$i];
-	if($line){
-		if(!$bdir){
-			$bdir = $line;
-		} else {
-			$bdir = "$bdir $line";
-		}
-	}
-}
-
-# Prep exclude
-my $exclude;
-for(my $i=0; $i < scalar(@exclude); $i++){
-	my $line = $exclude[$i];
-	if($line){
-		if(!$exclude){
-			$exclude = "--exclude=$line";
-		} else {
-			$exclude = "$exclude --exclude=$line";
-		}
-	}
-}
-
-# Clear last incremental directory
-my $tmp = time();
-my $tmpdir = "/tmp/$tmp-rback";
-
-my $ssh_extra;
-if($ssh_key || $ssh_known_hosts){
-	$ssh_extra = "-e \'ssh ";
-	if($ssh_known_hosts){
-		$ssh_extra .= "-o UserKnownHostsFile=$ssh_known_hosts ";
-	}
-
-	if($ssh_key){
-		$ssh_extra .= "-i $ssh_key ";
-	}
-
-	$ssh_extra .= "\'";
-}
-
-printlog("Incremental delete: Creating tmp dir $tmpdir");
-if(!-d $tmpdir){
-	mkdir $tmpdir, 0700;
-	my $cmd = "/usr/bin/rsync $ssh_extra --delete -a $tmpdir/ $ssh_user\@$server:$context/$weekday_name/";
-	printlog("Incremental delete: executing $cmd");
-	print "Incremental delete cmd: $cmd\n" if $debug;
-	open(CMD, "$cmd|");
-	close(CMD);
-	system("/bin/rm -rf $tmpdir");
-	printlog("Incremental delete: Removing $tmpdir");
-}
-
-# Create context dir if it does not exist
-if($context){
-	my $tmpdir2 = File::Temp->newdir();
-	my $cmd_context= "/usr/bin/rsync  $ssh_extra -r $tmpdir2 $ssh_user\@$server:$context";
-	print "$cmd_context\n" if $debug;
-	printlog("Syncing: $cmd_context");
-	open(CMD, "$cmd_context|");
-	close(CMD);
-}
-
-# Sync data
-my $cmd= "/usr/bin/rsync --delete-excluded $ssh_extra $exclude --backup --backup-dir=../$weekday_name --stats $options $bdir $ssh_user\@$server:$context/current > $outfile";
-print "$cmd\n" if $debug;
-printlog("Syncing: $cmd");
-open(CMD, "$cmd|");
-close(CMD);
-printlog("Syncing: Finished");
-if($email){
-	open(OUT, $outfile);
-	my $output = "Transfer summary:";
-	my $on=0;
-	while(<OUT>){
-		# Search for empty line and use the rest of the file 
-		# for transfer summary
-		chomp($_);
-		if($on){
-			$output = "$output\n$_";
-		}
-
-		if($_ eq ""){
-			$on=1;
-		}
-	}
-	close(OUT);
-
-	# Send email to backup admins
-	my $hostname = get_hostname();
-	$email_subject =~ s/{HOSTNAME}/$hostname/g;
-	sendmail($email_to, $email_from, $email_subject, $output);
-	printlog("Reporting: Sending email to $email_to from $email_from");
-}
-
-close(LOG);
-
-sub sendmail($$$$){
-	my($to, $from, $subject, $body) = @_;
-	open (MAIL, "|/usr/sbin/sendmail $to");
-		print MAIL "To: $to\n";
-		print MAIL "From: $from\n";
-		print MAIL "Subject: $subject\n\n";
-		print MAIL "$body";
-	close(MAIL);
-}
-
-sub getWeekDay($) {
-	my $val = $_[0];
-	if( $val > 6 ) {
-		return 0;
-	}
-	return (qw(sun mon tue wed thu fri sat))[$val];
-}
-
+# Logging
 sub printlog($){
 	my($text) = @_;
 	my ($sec,$min,$hour,$day,$month,$year) = gettime();
-	print LOG "$year-$month-$day $hour:$min:$sec $text\n";
+	my $out = "$year-$month-$day $hour:$min:$sec $text";
+
+	print LOG $out."\n";
+	if($o_verb){
+		print $out."\n";
+	} 
 }
 
+# Input validation
+sub check_options {
+	Getopt::Long::Configure ("bundling");
+	GetOptions(
+		'v'     => \$o_verb,            'verbose'	=> \$o_verb,
+		'h'     => \$o_help,            'help'	=> \$o_help,
+		'd'     => \$o_debug,            'debug'	=> \$o_debug,
+		't'     => \$o_test,            'test'	=> \$o_test,
+		'r'     => \$o_run,            'test'	=> \$o_run,
+	);
+
+	if(defined ($o_help)){
+		help();
+		exit 1;
+	}
+
+	if(defined($o_debug)){
+		$debug=1;
+	}
+	if(defined($o_test)){
+		$options .= " -n";
+		print "Dry run\n";
+	}
+
+	if(defined($o_run)){
+		# Run backup
+		RunBackup();
+	}
+}
+
+# Help
+sub help() {
+	print "$0\n";
+        print <<EOT;
+-v, --verbose
+        print extra debugging information
+-t, --test
+        Do not execute, only dry run
+-r, --run
+        Run backup
+-h, --help
+	print this help message
+EOT
+}
+
+sub print_usage() {
+        print "Usage: $0 [-v] ]\n";
+}
+
+
+# Process hosts to backup
+sub GetHash($){
+	my ($hash) = shift;
+
+	my @out;
+	my $i=0;
+	foreach my $dir (%$hash){
+		if($dir){
+			$out[$i] = $dir;
+			$i++;
+		}
+	}
+
+	return @out;
+}
+
+# Convert array to exclude string
+sub ArrayToExclude(@){
+	my (@dirs) = @_;
+
+	my $out;
+	foreach(@dirs){
+		$out .= "--exclude=\"$_\" ";
+	}
+	return $out;
+}
+
+# Convert array to string
+sub ArrayToString(@){
+	my (@dirs) = @_;
+
+	my $out;
+	foreach(@dirs){
+		$out .= "$_ ";
+	}
+	return $out;
+}
+
+# If dir does not exist, create it.
+sub DirExists($){
+	my ($dir) = @_;
+	if(!-e $dir){
+		printlog("Creating $dir");
+		mkdir $dir, 0700;
+	}
+}
+	
+# Convert array to include string
+sub ArrayToInclude(@){
+	my (@dirs) = @_;
+
+	my $out;
+	foreach(@dirs){
+		$out .= ":$_ ";
+
+	}
+	return $out;
+}
+
+# Get current time
 sub gettime(){
 	my $input = time();
 	my ($sec,$min,$hour,$day,$month,$year) = (localtime($input))[0,1,2,3,4,5];
@@ -202,9 +205,64 @@ sub gettime(){
 	return ($sec,$min,$hour,$day,$month,$year);
 }
 
-sub get_hostname(){
-	my $hostname = `/bin/hostname`;
-	chomp($hostname);
-	return $hostname;
+sub RunBackup(){
+	# Logging starts
+	open(LOG, ">>$logfile");
+	printlog("===========================");
+	printlog("Starting backup");
+
+	my $ssh_extra;
+	if($ssh_key || $ssh_known_hosts){
+		$ssh_extra = "-e \'ssh ";
+		if($ssh_known_hosts){
+			$ssh_extra .= "-o UserKnownHostsFile=$ssh_known_hosts ";
+		}
+
+		if($ssh_key){
+			$ssh_extra .= "-i $ssh_key ";
+		}
+
+		$ssh_extra .= "\'";
+	}
+
+	if(!-e $backup_dir){
+		printlog("Creating $backup_dir");
+		mkdir $backup_dir, 0700;
+	}
+
+
+	DirExists($logdir);
+	foreach my $host (keys %hosts){
+		my $user = $hosts{$host}{"user"};
+		printlog("Backing up $host with user $user");
+		if(!$user){
+			printlog("Error: user for $host does is missing");
+			next;
+		} 
+		my $host_backup_root = $backup_dir."/".$host;
+		my $host_backup_dir = $host_backup_root."/current/";
+		DirExists($host_backup_root);
+		DirExists($host_backup_dir);
+
+		my @include = GetHash($hosts{$host}{include});
+		my @exclude = GetHash($hosts{$host}{exclude});
+		my $include_str = ArrayToInclude(@include);
+		my $exclude_str = ArrayToExclude(@exclude);
+
+		my ($sec,$min,$hour,$day,$month,$year) = gettime();
+		my $backup_suffix = "$year-$month-$day-$hour-$min";
+
+		# Sync data
+		my $cmd= "/usr/bin/rsync --delete-excluded $ssh_extra ".$exclude_str." --backup --backup-dir=$host_backup_root/differential/$backup_suffix --stats $options $user\@".$host.$include_str." $host_backup_dir > $logdir/".$host."-".$backup_suffix.".log";
+		print "$cmd\n" if $debug;
+		printlog("Syncing: $cmd");
+		open(CMD, "$cmd|");
+		close(CMD);
+		printlog("Syncing: Finished");
+	}
+	printlog("Backup finished");
 }
 
+
+### Main
+check_options();
